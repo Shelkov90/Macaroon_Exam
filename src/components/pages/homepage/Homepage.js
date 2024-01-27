a07bfc8897fcb1c1607d18ff9d6fb936311aa8f3
Shelkov90 <wvablik90@ukr.net>
2024-01-27 12:26:10 +0200

    create components2

---
+++ src/components/pages/homepage/Homepage.js
@@ -0,0 +1,19 @@
+import React, { PureComponent } from 'react'
+
+class Homepage extends PureComponent {
+	constructor(props) {
+		super(props)
+
+		this.state = {
+			
+		}
+	}
+
+	render() {
+		return (
+			<div></div>
+		)
+	}
+}
+
+export default Homepage
