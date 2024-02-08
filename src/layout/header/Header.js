c866361344927e67040d0a7bcff5bd67bdaf11db
ArtHouse379 <artemchechel1997@gmail.com>
2024-02-08 16:44:16 +0200

    fix header

--- src/layout/header/Header.js
+++ src/layout/header/Header.js
@@ -18,19 +18,19 @@ class Header extends PureComponent {
             <img src="/images/leftArrow.svg" alt="alt" />
 
             <div className="header__banner__inner items-center flex">
-              {/* 		
-						<img className="mr-3" src="/images/banner1.png" alt="alt">
-						<p className="uppercase text-gray-600">доставка в день заказа</p> */}
+              		
+						<img className="mr-3" src="/images/banner1.png" alt="alt"/>
+						<p className="uppercase text-gray-600">доставка в день заказа</p>
 
-              <img className="mr-3" src="/images/banner2.png" alt="alt" />
+              {/* <img className="mr-3" src="/images/banner2.png" alt="alt" />
               <p className="uppercase text-gray-600">
                 оптовые поставки от производителя
               </p>
 
-              {/* <img className="mr-3" src="/images/banner3.png" alt="alt">
+              <img className="mr-3" src="/images/banner3.png" alt="alt"/>
 						<p className="uppercase text-gray-600">всегда свежее</p>
 
-						<img className="mr-3" src="/images/banner4.png" alt="alt">
+						<img className="mr-3" src="/images/banner4.png" alt="alt"/>
 						<p className="uppercase text-gray-600">миндальная мука и натуральные ингридиенты</p> */}
             </div>
 
